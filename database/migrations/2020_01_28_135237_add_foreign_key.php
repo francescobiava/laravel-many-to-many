722e71275022ba2bb4b33aa408f47b79ae426680
Francesco <franc.ns@gmail.com>
2020-01-28 15:01:01 +0100

    added migrations

---
+++ database/migrations/2020_01_28_135237_add_foreign_key.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employee_task', function(Blueprint $table) {
+            $table->bigInteger('employee_id')->unsigned()->index();
+            $table->foreign('employee_id', 'employee_task_employees')->references('id')->on('employees');
+            $table->bigInteger('task_id')->unsigned()->index();
+            $table->foreign('task_id', 'task_employee_tasks')->references('id')->on('tasks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employee_task', function(Blueprint $table) {
+            $table->dropForeing('employee_task_employees');
+            $table->dropForeign('task_employee_tasks');
+            $table->dropColumn('employee_id');
+            $table->dropColumn('task_id');
+        });
+    }
+}
