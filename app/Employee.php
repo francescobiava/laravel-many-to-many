77931ff7cee7e2dded3076ab2d223691b3a8b64b
Francesco <franc.ns@gmail.com>
2020-01-28 15:04:23 +0100

    added models

---
+++ app/Employee.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'firstname',
+        'lastname'
+    ];
+
+    public function tasks() {
+        return $this->belongsToMany(Task::class);
+    }
+}
